346f2851a990b788e3965189f54afa3ec9fcd169
pop9119 <drurymat@gmail.com>
2026-08-20 10:26:22 -0400

    Added a nice priority
    Added the process priority to the kill command to ensure that the hanging Unbound process doesn't prevent the script from running.

--- src/usr/local/www/scripts/restart_unbound.sh
+++ src/usr/local/www/scripts/restart_unbound.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# Force kill all unbound processes
-killall -9 unbound
-# Wait for processes to release resources
+# Force kill all unbound processes with maximum scheduling priority
+# nice -n -20 gives the process the highest non-realtime priority
+nice -n -20 killall -9 unbound
+
+# Wait for processes to release resources (7 seconds)
 sleep 7
+
 # Restart the unbound service
 /usr/local/sbin/unbound -d -f /usr/local/etc/unbound.conf
